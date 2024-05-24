b05c0432f36291401f0099e7a377bb36371df9f8
SirEvgen <sandrofa6666666@gmail.com>
2024-05-24 16:46:59 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,6 @@
+my_list = ['apple', 'orange', 'coconute', 'nut', 'pineapple', 'banana']
+print(my_list)
+print(my_list[::3])
+print(my_list[2:5])
+my_list[2] = 'pear'
+print(my_list)
---
+++ homework6.py
@@ -0,0 +1,5 @@
+my_dict = {'grass': 'трава', 'tree': 'дерево', 'open': 'открывать'}
+print(my_dict)
+print(my_dict.get('tree'))
+my_dict.update({'bird': 'птица','bear': 'медведь'})
+print(my_dict)
